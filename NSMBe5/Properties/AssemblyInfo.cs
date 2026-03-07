f91c1d8d8ebbd4b0b3ed98aa80c55c6899fd0806
David <141585041+David-Uzno@users.noreply.github.com>
2026-03-06 21:05:02 -0300

    chore: cambio de nombre a "NSMB Editor Reloaded"
    Actualizar el nombre de la aplicación para una marca consistente: se cambió _TITLE a "NSMB Editor Reloaded" en los archivos de idioma INI en inglés, holandés, francés, portugués y español, y se actualizó AssemblyTitle en NSMBe5/Properties/AssemblyInfo.cs.

--- NSMBe5/Properties/AssemblyInfo.cs
+++ NSMBe5/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NSMB Editor 5.4")]
+[assembly: AssemblyTitle("NSMB Editor Reloaded")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
